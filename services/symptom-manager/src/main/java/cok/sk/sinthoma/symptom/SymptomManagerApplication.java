d0b48c6593f3dede0347f0627c022b5444980787
shishir-insane <shishir.insane@gmail.com>
2019-07-24 11:00:51 +0200

    First Light

---
+++ services/symptom-manager/src/main/java/cok/sk/sinthoma/symptom/SymptomManagerApplication.java
@@ -0,0 +1,13 @@
+package cok.sk.sinthoma.symptom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymptomManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymptomManagerApplication.class, args);
+	}
+
+}

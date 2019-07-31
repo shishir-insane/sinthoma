7b14f645afa0a880462864d74db891dafd74ff2b
shishir-insane <shishir.insane@gmail.com>
2019-07-31 14:55:49 +0200

    Restructured and renamed

---
+++ web/sinthoma-web/src/main/java/com/sk/sinthoma/web/SinthomaWebApplication.java
@@ -0,0 +1,30 @@
+/**
+ * SinthomaWebApplication.java
+ * sinthoma-web
+ * Copyright 2019 Shishir Kumar
+ * 
+ * Licensed under the GNU Lesser General Public License v3.0
+ * Permissions of this license are conditioned on making available complete 
+ * source code of licensed works and modifications under the same license 
+ * or the GNU GPLv3. Copyright and license notices must be preserved. 
+ * 
+ * Contributors provide an express grant of patent rights. However, a larger 
+ * work using the licensed work through interfaces provided by the licensed 
+ * work may be distributed under different terms and without source code for 
+ * the larger work.
+ */
+package com.sk.sinthoma.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.sk.sinthoma.web", "com.sk.sinthoma.web.user"})
+public class SinthomaWebApplication {
+
+    public static void main(String[] args) {
+	SpringApplication.run(SinthomaWebApplication.class, args);
+    }
+
+}
---
+++ web/sinthoma-web/src/main/java/com/sk/sinthoma/web/config/AppSecurityConfig.java
@@ -0,0 +1,82 @@
+/**
+ * SecurityJavaConfig.java
+ * sinthoma-web
+ * Copyright 2019 Shishir Kumar
+ *
+ * Licensed under the GNU Lesser General Public License v3.0
+ * Permissions of this license are conditioned on making available complete
+ * source code of licensed works and modifications under the same license
+ * or the GNU GPLv3. Copyright and license notices must be preserved.
+ *
+ * Contributors provide an express grant of patent rights. However, a larger
+ * work using the licensed work through interfaces provided by the licensed
+ * work may be distributed under different terms and without source code for
+ * the larger work.
+ */
+package com.sk.sinthoma.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.sk.sinthoma.web.user.security.LoggingAccessDeniedHandler;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    private LoggingAccessDeniedHandler accessDeniedHandler;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+	auth.inMemoryAuthentication()
+		.withUser("admin")
+			.password(encoder().encode("adminPass"))
+			.roles("ADMIN")
+		.and()
+		.withUser("user")
+			.password(encoder().encode("userPass"))
+			.roles("USER");
+    }
+
+    @Bean
+    public PasswordEncoder encoder() {
+	return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                    .antMatchers(
+                            "/css/**",
+                            "/js/**",
+                            "/images/**",
+                            "/fonts/**",
+                            "/webjars/**").permitAll()
+                    .antMatchers("/").hasRole("USER")
+                    .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                .and()
+                .logout()
+                    .invalidateHttpSession(true)
+                    .clearAuthentication(true)
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/login?logout")
+                    .permitAll()
+                .and()
+                .exceptionHandling()
+                    .accessDeniedHandler(accessDeniedHandler);
+    }
+}

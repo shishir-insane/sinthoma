de0e9e58f0aaa51cf50a4f39c01fc41438031fc3
shishir-insane <shishir.insane@gmail.com>
2019-08-02 14:12:48 +0200

    Code indentation

--- service/user-manager/src/test/java/com/sk/sinthoma/user/model/UserTest.java
+++ service/user-manager/src/test/java/com/sk/sinthoma/user/model/UserTest.java
@@ -1,3 +1,18 @@
+/**
+ * UserTest.java
+ * user-manager
+ * Copyright 2019 Shishir Kumar
+ * 
+ * Licensed under the GNU Lesser General Public License v3.0
+ * Permissions of this license are conditioned on making available complete 
+ * source code of licensed works and modifications under the same license 
+ * or the GNU GPLv3. Copyright and license notices must be preserved. 
+ * 
+ * Contributors provide an express grant of patent rights. However, a larger 
+ * work using the licensed work through interfaces provided by the licensed 
+ * work may be distributed under different terms and without source code for 
+ * the larger work.
+ */
 package com.sk.sinthoma.user.model;
 
 import org.junit.Test;
@@ -8,6 +23,6 @@ public class UserTest {
 
     @Test
     public void equalsContract() {
-        EqualsVerifier.forClass(User.class).verify();
+	EqualsVerifier.forClass(User.class).verify();
     }
 }
--- service/user-manager/src/test/java/com/sk/sinthoma/user/repository/UserRepositoryTest.java
+++ service/user-manager/src/test/java/com/sk/sinthoma/user/repository/UserRepositoryTest.java
@@ -1,3 +1,18 @@
+/**
+ * UserRepositoryTest.java
+ * user-manager
+ * Copyright 2019 Shishir Kumar
+ * 
+ * Licensed under the GNU Lesser General Public License v3.0
+ * Permissions of this license are conditioned on making available complete 
+ * source code of licensed works and modifications under the same license 
+ * or the GNU GPLv3. Copyright and license notices must be preserved. 
+ * 
+ * Contributors provide an express grant of patent rights. However, a larger 
+ * work using the licensed work through interfaces provided by the licensed 
+ * work may be distributed under different terms and without source code for 
+ * the larger work.
+ */
 package com.sk.sinthoma.user.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -10,13 +25,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
-import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import com.mongodb.BasicDBObjectBuilder;
-import com.mongodb.DBObject;
 import com.sk.sinthoma.user.model.User;
 
 @DataMongoTest
@@ -28,7 +38,7 @@ public class UserRepositoryTest {
 
     @BeforeEach
     public void init() {
-	User user = new User();
+	final User user = new User();
 	user.setFirstName("John");
 	user.setLastName("Doe");
 	userRepository.save(user);
@@ -36,7 +46,7 @@ public class UserRepositoryTest {
 
     @Test
     void testFindByFirstNameAndLastNameNotNulResult() {
-	List<User> users = userRepository.findByFirstNameAndLastName("John", "Doe");
+	final List<User> users = userRepository.findByFirstNameAndLastName("John", "Doe");
 	assertThat(users).isNotNull();
 	assertThat(users.size()).isEqualTo(1);
     }

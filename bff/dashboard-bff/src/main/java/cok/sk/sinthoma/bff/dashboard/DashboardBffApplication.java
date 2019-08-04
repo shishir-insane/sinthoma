2b51bb43713386de0ac1cccc2e09bf8c6464ed07
shishir-insane <shishir.insane@gmail.com>
2019-08-04 14:26:49 +0200

    Updated doc

--- bff/dashboard-bff/src/main/java/cok/sk/sinthoma/bff/dashboard/DashboardBffApplication.java
+++ bff/dashboard-bff/src/main/java/cok/sk/sinthoma/bff/dashboard/DashboardBffApplication.java
@@ -1,3 +1,18 @@
+/**
+ * DashboardBffApplication.java
+ * dashboard-bff
+ * Copyright 2019 Shishir Kumar
+ * 
+ * Licensed under the GNU Lesser General Public License v3.0
+ * Permissions of this license are conditioned on making available complete 
+ * source code of licensed works and modifications under the same license 
+ * or the GNU GPLv3. Copyright and license notices must be preserved. 
+ * 
+ * Contributors provide an express grant of patent rights. However, a larger 
+ * work using the licensed work through interfaces provided by the licensed 
+ * work may be distributed under different terms and without source code for 
+ * the larger work.
+ */
 package cok.sk.sinthoma.bff.dashboard;
 
 import org.springframework.boot.SpringApplication;
@@ -6,8 +21,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DashboardBffApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DashboardBffApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(DashboardBffApplication.class, args);
+    }
 
 }

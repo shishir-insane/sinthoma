2722616ff789ed1c3ad378e6eaf6ff166b78fc74
shishir-insane <shishir.insane@gmail.com>
2019-08-21 17:25:14 +0200

    Update to Spring Boot 2.2

--- bff/dashboard-bff/src/main/java/com/sk/sinthoma/bff/dashboard/log/ApplicationLogAspect.java
+++ bff/dashboard-bff/src/main/java/com/sk/sinthoma/bff/dashboard/log/ApplicationLogAspect.java
@@ -4,10 +4,7 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 
-import lombok.extern.slf4j.Slf4j;
-
 @Aspect
-@Slf4j
 public class ApplicationLogAspect {
     
     @Pointcut("within(com.sk.sinthoma.bff.dashboard.controller.*)")

dd14e8856af8fa20383422626a1a3d730265c890
shishir-insane <shishir.insane@gmail.com>
2019-08-28 14:21:47 +0200

    Working draft of authentication server

--- core/core-auth-service/src/main/java/com/sk/sinthoma/core/auth/rest/DefaultUserManagerRestService.java
+++ core/core-auth-service/src/main/java/com/sk/sinthoma/core/auth/rest/DefaultUserManagerRestService.java
@@ -4,9 +4,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import com.sk.sinthoma.core.auth.model.User;
@@ -16,24 +16,36 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 @Slf4j
 public class DefaultUserManagerRestService implements UserService {
-    
+
     @Value("#{'${app.services.user-manager.uri}' + " + "'${app.services.user-manager.resource.search-by-username}'}")
     private String userManagerSearchResourceUri;
-    
+
+    @Value("#{'${app.services.user-manager.uri}' + " + "'${app.services.user-manager.resource.user}'}")
+    private String userManagerResourceUri;
+
     @Autowired
     private RestTemplate restTemplate;
 
     @Override
     public Optional<User> findByUserName(String userName) {
+	User user = null;
 	if (StringUtils.isEmpty(userName)) {
 	    throw new IllegalArgumentException("UserName in the request can not be empty.");
 	}
-	final User loggedUser = restTemplate.getForObject(userManagerSearchResourceUri, User.class, userName);
-	if ((null == loggedUser) || StringUtils.isEmpty(loggedUser.getUsername())) {
-	    log.warn("Username {} trying to login is invalid.", userName);
-	    throw new UsernameNotFoundException("User trying to login is invalid.");
+	try {
+	    user = restTemplate.getForObject(userManagerSearchResourceUri, User.class, userName);
+	} catch (RestClientException e) {
+	   log.warn("Error while fetching details for requested user '{}'.", userName, e);
 	}
-	return Optional.of(loggedUser);
+	return Optional.ofNullable(user);
+    }
+
+    @Override
+    public User saveNewUser(User user) {
+	if (null == user || StringUtils.isEmpty(user.getUsername())) {
+	    throw new IllegalArgumentException("User object or UserName in the request can not be empty.");
+	}
+	return restTemplate.postForObject(userManagerResourceUri, user, User.class);
     }
 
 }
